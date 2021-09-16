97524dd0ab3cc2b39153da3c26ccbb4cc1135e27
kmataloni <81983413+kmataloni@users.noreply.github.com>
2021-09-16 08:53:15 -0500

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,5 @@
 // from data.js
-var tableData = data;
+// var tableData = data;
 
 // YOUR CODE HERE!
 var tbody = d3.select("tbody");
@@ -9,12 +9,42 @@ var city = d3.select("#city");
 var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
 
 var populate = (dataInput) => {
+    tbody.html("");
     dataInput.forEach(ufo_data => {
         var row = tbody.append("tr");
         columns.forEach(column => row.append("td").text(ufo_data[column]))
         
     });
+
+
+
 }
 
 populate(data);
+
+function handleClick() {
+    var userdate = d3.select("#datetime").property("value");
+    var filteredData = data;
+
+
+    console.log("A button was clicked!");
+      // We can use d3 to see the object that dispatched the event
+    
+    console.log(d3.event.target.value);
+    console.log(userdate);
+
+    // check for user date
+    if(userdate) {
+        // filter data
+        filteredData = filteredData.filter(row => row.datetime == userdate);
+        console.log(filteredData);
+
+    }
+
+    populate(filteredData);
+}
+
+
+// We can use the `on` function in d3 to attach an event to the handler function
+button.on("click", handleClick);
 
